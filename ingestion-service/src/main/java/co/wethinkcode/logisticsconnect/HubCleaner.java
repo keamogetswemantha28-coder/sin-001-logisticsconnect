d105342b746f1ade4e695aefeaea26032865a129
Keamogetswe Samantha Mokalapa <kemokjhb025@student.wethinkcode.co.za>
2026-08-29 21:47:58 +0200

    feat: add boolean normalisation implementation

--- ingestion-service/src/main/java/co/wethinkcode/logisticsconnect/HubCleaner.java
+++ ingestion-service/src/main/java/co/wethinkcode/logisticsconnect/HubCleaner.java
@@ -9,7 +9,15 @@ public class HubCleaner {
         return text.replaceAll("\\s+", " ").trim();
     }
 
-    public String normaliseBoolean(String text){
-        return "";
+    public String normaliseBoolean(String value){
+        Set<String> trueValues = Set.of("1", "true", "yes", "y");
+        Set<String> falseValues = Set.of("0", "false", "n", "no");
+        if ((trueValues.contains(value.toLowerCase()))){
+            return "true";
+        }
+        else if (falseValues.contains(value.toLowerCase())){
+            return "false";
+        }
+        return null;
     }
 }
