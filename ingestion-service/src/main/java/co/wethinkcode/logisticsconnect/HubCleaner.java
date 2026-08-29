d377f59a6804adcf829fa1f7c20f4ec313fa070f
Keamogetswe Samantha Mokalapa <kemokjhb025@student.wethinkcode.co.za>
2026-08-29 18:43:19 +0200

    test: add tests for Boolean normalisation

--- ingestion-service/src/main/java/co/wethinkcode/logisticsconnect/HubCleaner.java
+++ ingestion-service/src/main/java/co/wethinkcode/logisticsconnect/HubCleaner.java
@@ -1,9 +1,15 @@
 package co.wethinkcode.logisticsconnect;
 
+import java.util.Set;
+
 public class HubCleaner {
 
     //Remove trailing spaces and inner spaces
     public String cleanWhiteSpace(String text){
         return text.replaceAll("\\s+", " ").trim();
+    }
+
+    public String normaliseBoolean(String text){
+        return "";
     }
 }
--- ingestion-service/src/test/java/co/wethinkcode/logisticsconnect/HubCleanerTest.java
+++ ingestion-service/src/test/java/co/wethinkcode/logisticsconnect/HubCleanerTest.java
@@ -3,6 +3,7 @@ package co.wethinkcode.logisticsconnect;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HubCleanerTest {
 
@@ -22,5 +23,30 @@ public class HubCleanerTest {
         String results = cleaner.cleanWhiteSpace("Johannesburg  Hub");
 
         assertEquals("Johannesburg Hub", results);
+    }
+
+//    "TRUE" → "true"
+//            "1" → "true"
+//            "Y" → "true"
+//            "yes" → "true"
+//            "FALSE" → "false"
+//            "0" → "false"
+//            "N" → "false"
+//            "no" → "false"
+
+    @Test
+    void normaliseBoolean_ReturnsTheCorrectValue(){
+        HubCleaner hubCleaner = new HubCleaner();
+
+        //String results = hubCleaner.normaliseBoolean("TRUE");
+
+        assertEquals("true", hubCleaner.normaliseBoolean("TRUE"));
+        assertEquals("true", hubCleaner.normaliseBoolean("1"));
+        assertEquals("true", hubCleaner.normaliseBoolean("yes"));
+        assertEquals("true", hubCleaner.normaliseBoolean("Y"));
+        assertEquals("false", hubCleaner.normaliseBoolean("0"));
+        assertEquals("false", hubCleaner.normaliseBoolean("FALSE"));
+        assertEquals("false", hubCleaner.normaliseBoolean("N"));
+        assertEquals("false", hubCleaner.normaliseBoolean("no"));
     }
 }
