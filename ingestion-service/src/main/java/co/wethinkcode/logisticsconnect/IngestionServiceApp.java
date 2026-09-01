d636c9bd999cc377c75f4922180f920a34937838
Keamogetswe Samantha Mokalapa <kemokjhb025@student.wethinkcode.co.za>
2026-09-02 01:24:33 +0200

    feat: add GET/ hubs emdpoint exposing cleaned hub data as JSON

--- ingestion-service/src/main/java/co/wethinkcode/logisticsconnect/IngestionServiceApp.java
+++ ingestion-service/src/main/java/co/wethinkcode/logisticsconnect/IngestionServiceApp.java
@@ -2,12 +2,23 @@ package co.wethinkcode.logisticsconnect;
 
 import io.javalin.Javalin;
 
+import java.util.List;
+
 public class IngestionServiceApp {
 
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7050);
 
         app.get("/health", ctx -> ctx.result("OK"));
+
+        //Add hub endpoint
+        app.get("/hubs", context -> {
+            HubCsvReader reader = new HubCsvReader();
+            List<Hub> hubs = reader.readAndClean();
+            context.json(hubs);
+
+        });
+
 
         // TODO: read and clean src/main/resources/hubs-global.csv (hubs, sorting centers, regional districts data —
         // trim whitespace, fix casing, normalize dates/booleans) and expose the
