bf45917186c5e1e761bd4e17f4b4f55d139b8586
Keamogetswe Samantha Mokalapa <kemokjhb025@student.wethinkcode.co.za>
2026-09-01 03:12:35 +0200

    feat: implement HubCsvReader to read and clean hubs-global.csv

--- ingestion-service/src/main/java/co/wethinkcode/logisticsconnect/HubCsvReader.java
+++ ingestion-service/src/main/java/co/wethinkcode/logisticsconnect/HubCsvReader.java
@@ -13,6 +13,25 @@ public class HubCsvReader {
         HubCleaner cleaner = new HubCleaner();
         //New list to store cleaned data
         List<Hub> hubs = new ArrayList<>();
+        try(InputStream stream = getClass().getClassLoader().getResourceAsStream("hubs-global.csv");
+            CSVReader reader = new CSVReader(new InputStreamReader(stream))){
+
+            reader.readNext();
+            String [] row;
+            while ((row = reader.readNext()) != null){
+                String hubId = cleaner.hubIdToUpperCase(cleaner.cleanWhiteSpace(row[0]));
+                String province = cleaner.toTitleCase(cleaner.cleanWhiteSpace(row[1]));
+                String sortingCenter = cleaner.cleanWhiteSpace(row[2]);
+                String activeStr = cleaner.normaliseBoolean(cleaner.cleanWhiteSpace(row[3]));
+                boolean active = "true".equals(activeStr);
+
+                Hub hub = new Hub(hubId, province,sortingCenter,active);
+                hubs.add(hub);
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return hubs;
     }
 }
