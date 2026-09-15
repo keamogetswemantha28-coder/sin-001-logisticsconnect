4c79b855c280d7bbfa959642d2095a02fd24b852
Keamogetswe Samantha Mokalapa <keamogetswemantha28@gmail.com>
2026-09-16 01:46:24 +0200

    Refactor(HubCsvReader): prevent one messy row from breaking the whole file

--- ingestion-service/src/main/java/co/wethinkcode/logisticsconnect/HubCsvReader.java
+++ ingestion-service/src/main/java/co/wethinkcode/logisticsconnect/HubCsvReader.java
@@ -1,7 +1,9 @@
 package co.wethinkcode.logisticsconnect;
 
 import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -9,41 +11,46 @@ import java.util.List;
 
 public class HubCsvReader {
 
-    public List<Hub> readAndClean(){
+    public List<Hub> readAndClean() {
         HubCleaner cleaner = new HubCleaner();
         //New list to store cleaned data
         List<Hub> hubs = new ArrayList<>();
-        try(InputStream stream = getClass().getClassLoader().getResourceAsStream("hubs-global.csv");
-            CSVReader reader = new CSVReader(new InputStreamReader(stream))){
+        try (InputStream stream = getClass().getClassLoader().getResourceAsStream("hubs-global.csv");
+             CSVReader reader = new CSVReader(new InputStreamReader(stream))) {
 
             reader.readNext();
-            String [] row;
-            while ((row = reader.readNext()) != null){
-                String hubId = cleaner.hubIdToUpperCase(cleaner.cleanWhiteSpace(row[0]));
+            String[] row;
+            while ((row = reader.readNext()) != null) {
+                try{
+                    String hubId = cleaner.hubIdToUpperCase(cleaner.cleanWhiteSpace(row[0]));
 
-                String rawProvince = cleaner.cleanWhiteSpace(row[1]);
-                String province = cleaner.placeholderValues(rawProvince);
+                    String rawProvince = cleaner.cleanWhiteSpace(row[1]);
+                    String province = cleaner.placeholderValues(rawProvince);
 
-                if (province != null){
-                    province = cleaner.normaliseProvince(province);
+                    if (province != null) {
+                        province = cleaner.normaliseProvince(province);
+                    }
+                    String sortingCenter = cleaner.cleanWhiteSpace(row[2]);
+                    String activeStr = cleaner.normaliseBoolean(cleaner.cleanWhiteSpace(row[3]));
+
+                    Boolean active;
+                    if (activeStr == null) {
+                        active = null;
+                    } else {
+                        active = Boolean.valueOf(activeStr);
+                    }
+
+                    Hub hub = new Hub(hubId, province, sortingCenter, active);
+                    hubs.add(hub);
+                } catch (Exception e) {
+                    System.out.println("Skipping row: " + String.join(",", row) + ";" + e.getMessage());
                 }
-                String sortingCenter = cleaner.cleanWhiteSpace(row[2]);
-                String activeStr = cleaner.normaliseBoolean(cleaner.cleanWhiteSpace(row[3]));
-
-                Boolean active;
-                if (activeStr == null){
-                    active = null;
-                }else {
-                    active = Boolean.valueOf(activeStr);
-                }
-
-                Hub hub = new Hub(hubId, province,sortingCenter,active);
-                hubs.add(hub);
 
             }
-        }catch (Exception e){
+        } catch (IOException | CsvException e) {
             e.printStackTrace();
         }
         return hubs;
     }
+
 }
