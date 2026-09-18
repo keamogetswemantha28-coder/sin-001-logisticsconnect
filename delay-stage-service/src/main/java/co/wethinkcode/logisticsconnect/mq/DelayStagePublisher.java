38708fb92025c9d4f37cb82f441f0676ca78fcef
Keamogetswe Samantha Mokalapa <keamogetswemantha28@gmail.com>
2026-09-18 22:43:43 +0200

    feat(DelayStagePublisher): add ActiveMQ publisher for stage changes

--- delay-stage-service/src/main/java/co/wethinkcode/logisticsconnect/mq/DelayStagePublisher.java
+++ delay-stage-service/src/main/java/co/wethinkcode/logisticsconnect/mq/DelayStagePublisher.java
@@ -1,4 +1,29 @@
 package co.wethinkcode.logisticsconnect.mq;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+import javax.jms.*;
+
 public class DelayStagePublisher {
+
+    public void publish(String hubId, int stage) {
+        try {
+            ConnectionFactory factory = new ActiveMQConnectionFactory(MqConfig.BROKER_URL);
+            Connection connection = factory.createConnection();
+            connection.start();
+
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Topic topic = session.createTopic(MqConfig.TOPIC);
+            MessageProducer producer = session.createProducer(topic);
+
+            String json = String.format(
+                    "{\"hubId\": \"%s\", \"stage\": %d}", hubId, stage
+            );
+            TextMessage message = session.createTextMessage(json);
+            producer.send(message);
+
+            connection.close();
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
 }
